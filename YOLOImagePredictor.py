8037538b34e60b9934643599e0e3a86bc2a91775
Hyeonwoo Heo <hyeonwoo.heo@gmail.com>
2024-05-29 15:27:32 +0900

    renamed

--- YOLOImagePredictor.py
+++ YOLOImagePredictor.py
@@ -18,7 +18,7 @@ class YOLOImagePredictor:
                 im_array = result.plot()
                 im = Image.fromarray(im_array[..., ::-1])
                 original_filename = os.listdir(self.directory)[i]
-                result_filename = os.path.splitext(original_filename)[0] + f'_result_.jpg'
+                result_filename = os.path.splitext(original_filename)[0] + f'_result.jpg'
                 im.save(os.path.join(self.save_dir, result_filename))
         
         print(f"Predictions saved in {self.save_dir}" +
